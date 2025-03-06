1979ac08fa44e072018db841f74f155ec7abccae
chanchan2 <106582155+chanchan2hi@users.noreply.github.com>
2025-03-06 16:14:17 +0900

    커밋

---
+++ Assets/Scripts/ObjectDectector.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDectector : MonoBehaviour
+{
+  [SerializeField]private TowerSpawner towerSpawner;
+  private Camera mainCamera;
+  private Ray ray;
+  private RaycastHit hit;
+
+  private void Awake()
+  {
+    //"mainCamera"태그를 가지고 있는 오브젝트 탐색 후 카메라 컴포넌트 정보 전달
+    //GameObject.FindGameObjectTag("mainCamera").GetComponent<Camera>(); 와동일
+    mainCamera = Camera.main;
+  }
+
+  private void Update()
+  {
+    // 마우스 왼쪽 버튼을 눌렀을 떄
+    if (Input.GetMouseButtonDown(0))
+    {
+      //카메라 위치에서 화면의 마우스 위치를 관통하는 광선 생선
+      //ray.origin: 광선의 시작위치(=카메라위치)
+      //ray.direction :광선의 진행방향
+      ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+      //2D모니터를 통해 3D월드의 오브젝트를 마우스로 선택하는 방법
+      //광선에 부딪히는 오브젝트를 검출하여 hit에 저장
+      if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+      {
+        Debug.Log("태그 전");
+        //광선에 부딪힌 오브젝트의 태그가 "TILE"이면
+        if (hit.transform.CompareTag("Tile"))
+        {//
+          Debug.Log("태그 후");
+         //타워를 생성하는 SpawnerTower()호출
+         towerSpawner.SpawnTower(hit.transform);
+        }
+      }
+    }
+  }
+}
